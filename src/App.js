6d0b0f44b3f151ffb0e629986e20cc31096c1138
ironpup <christoph.ding@gmail.com>
2019-01-17 15:09:12 -0500

    Stub out the components I believe will make up this landing page.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+
+}
+
+class Logo extends Component {
+
+}
+
+class MiddleSplash extends Component {
+  
+}
+
+class Description extends Component {
+
+}
+
+class Quotes extends Component {
+
+}
+
+class Contact extends Component {
+
+}
+
+class Footer extends Component {
+
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1> Xavier </h1>
       </div>
     );
   }
 }
 
 export default App;
+
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
